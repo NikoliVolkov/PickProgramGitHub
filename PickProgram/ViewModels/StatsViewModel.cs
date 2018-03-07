00a2b5dfa87cfec1daa17d44737b98a0d6408c26
Nick <Nick@POSEIDON>
2018-03-07 00:54:55 -0800

    dont use draw event, broke timers, now calling init timers and update dll on delete and close ticket functions, dynamic stat section in the works, had to use a different selector to change the heading on the offsite table since there is now another table on the page

---
+++ PickProgram/ViewModels/StatsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PickProgram.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace PickProgram.ViewModels
+{
+    public class StatsViewModel
+    {
+        public int InvoiceId { get; set; }
+        public List<SelectListItem> Employees { get; set; }
+    }
+
+}
