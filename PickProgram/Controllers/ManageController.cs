c4c42823cc741ce8ef31023e627c86f24f2dae77
Nick <Nick@POSEIDON2>
2018-06-07 20:51:35 -0700

    adding manage personnel page

---
+++ PickProgram/Controllers/ManageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PickProgram.Models;
+using PickProgram.ViewModels;
+
+namespace PickProgram.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public ManageController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public IActionResult Personnel()
+        {
+            //var completedInvoices = _invoiceRepository.GetAllCompletedInvoicesLast30().ToList();
+            //var avm = new ArchiveViewModel(){ };
+            return View();
+        }
+    }
+}
