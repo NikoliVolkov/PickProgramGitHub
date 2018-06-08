923ee3af5acd28896cc88ff7cf0fb41047c94e93
Nick <Nick@POSEIDON2>
2018-06-07 19:47:18 -0700

    simplified procedure for getting unassigned employees

--- InvoiceTracker/dbo/Stored Procedures/pGetUnassignedEmployees.sql
+++ InvoiceTracker/dbo/Stored Procedures/pGetUnassignedEmployees.sql
@@ -10,12 +10,6 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SELECT DISTINCT [EmployeeId], [FirstName], [LastName], [Nickname] 
-	--get employee id's for currently assigned tickets
+	SELECT [EmployeeId], [FirstName], [LastName], [Nickname] 
 		FROM dbo.Employee e WHERE [EmployeeId] NOT IN (SELECT [AssignedEmployeeId] FROM dbo.Invoice WHERE [StatusId] = 1 AND [AssignedEmployeeId] IS NOT NULL)
-		UNION
-	--new employees who have never been assigned
-	SELECT DISTINCT [EmployeeId], [FirstName], [LastName], [Nickname] 
-		FROM dbo.Employee e LEFT JOIN dbo.Invoice i on i.[AssignedEmployeeId] = e.[EmployeeId]
-		LEFT JOIN dbo.InvoiceStatus invs ON i.[StatusId] = invs.[StatusId] WHERE i.[StatusId] IS NULL
 END
